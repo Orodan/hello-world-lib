ba22c38a36c3d620fe0ac575b8c15c83c33fc6c0
Jimmy Kasprzak <jimmy.kasprzak@afp.com>
2017-12-12 12:24:11 +0100

    First commit

---
+++ demo/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HelloWorldModule } from '../../../dist/hello-world.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HelloWorldModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
