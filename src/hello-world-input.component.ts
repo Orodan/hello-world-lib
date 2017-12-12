61ab487efde65e4c78d279cab141dea0b02eef9b
Jimmy Kasprzak <jimmy.kasprzak@afp.com>
2017-12-12 16:39:44 +0100

    Add hello world input.

---
+++ src/hello-world-input.component.ts
@@ -0,0 +1,32 @@
+import { Component, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'hello-world-input',
+  template: '<input type="text" class="form-control">',
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => HelloWorldInputComponent),
+    multi: true
+  }]
+})
+export class HelloWorldInputComponent implements ControlValueAccessor {
+
+  data: any;
+
+  onChange = (_) => { };
+  onTouched = () => { };
+
+  registerOnChange (fn: (_: any) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched (fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  writeValue (value: any): void {
+    this.data = value;
+  }
+
+}
